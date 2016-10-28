db15e65eaf50ffc70251eb856b93a186dc0b10f4
chinamobile <yanxuexu007@126.com>
2016-10-28 14:52:34 +0800

    准备测试

--- src/test/java/cm/spider/redis/test/spiderToRedisTest.java
+++ src/test/java/cm/spider/redis/test/spiderToRedisTest.java
@@ -7,6 +7,13 @@ public class spiderToRedisTest {
 	public static Logger logger=Logger.getLogger(spiderToRedisTest.class);
 	
 	public static void main(String[] args) {
-		
+		try{
+			logger.info(" spiderToRedisTest starts ");
+			
+			
+			logger.info(" spiderToRedisTest ends ");
+		}catch(Exception ex){
+			logger.info(" spiderToRedisTest crashes :"+ex.getMessage());
+		}
 	}
 }
