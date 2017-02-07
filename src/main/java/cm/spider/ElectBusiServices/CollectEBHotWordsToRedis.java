9d598b707226cfe11ab981188426fbc24d6e784a
chinamobile <yanxuexu007@126.com>
2017-02-07 11:20:49 +0800

    hahaha

--- src/main/java/cm/spider/ElectBusiServices/CollectEBHotWordsToRedis.java
+++ src/main/java/cm/spider/ElectBusiServices/CollectEBHotWordsToRedis.java
@@ -12,7 +12,7 @@ import cm.redis.Commons.RedisClusterObj;
 import cm.redis.Commons.TimeFormatter;
 //import cm.spider.CrawlerControllers.ChineseWordsAmazonCrawlController;
 import cm.spider.CrawlerControllers.ChineseWordsJingDongCrawlController;
-import cm.spider.CrawlerControllers.ChineseWordsSuNingYiGouCrawlController;
+//import cm.spider.CrawlerControllers.ChineseWordsSuNingYiGouCrawlController;
 import cm.spider.CrawlerControllers.ChineseWordsTaoBaoCrawlController;
 import cm.spider.CrawlerControllers.ChineseWordsVIPCrawlController;
 import cm.spider.CrawlerControllers.ChineseWordsYiHaoDianCrawlController;
